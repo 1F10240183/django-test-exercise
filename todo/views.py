ee00d433a702bc55f6cfa8e3729818da1fdd293c
1F10240183 <s1f102401832@iniad.org>
2025-07-03 13:56:18 +0900

    エラーを修正

--- todo/views.py
+++ todo/views.py
@@ -5,13 +5,17 @@ from todo.models import Task
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        due_at = request.POST.get('due_at')
+        if title:
+            Task.objects.create(title=title, due_at=due_at or None)
+            # return redirect('/')  # テストのためコメントアウト
+
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at'),
+        tasks = Task.objects.order_by('due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-
-    context = {
-        'tasks':tasks
-    }
-    return render(request,'todo/index.html', context)
+    context = {'tasks': tasks}
+    return render(request, 'todo/index.html', context)
